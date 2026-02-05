2440a21485e45b9393d53a41b4932481c9ef6eb4
Khaled Waleed <khaledw62@gmail.com>
2026-02-05 18:27:43 +0200

    feat: DataFast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Cairo } from "next/font/google";
 import "./globals.css";
 import { LayoutContent } from "./LayoutContent";
 import ScrollManager from "@/app/components/RefreshScrollRestoration";
+import Script from "next/script";
 
 const cairo = Cairo({
   subsets: ["latin"],
@@ -23,8 +24,16 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          data-website-id="dfid_CgRWCeHd3DFpRuCvrzfJR"
+          data-domain="codetoon.net"
+          src="https://datafa.st/js/script.js"
+        />
+      </head>
       <body className={`${cairo.variable} font-sans antialiased`}>
-        <ScrollManager/>
+        <ScrollManager />
         <LayoutContent>{children}</LayoutContent>
       </body>
     </html>
