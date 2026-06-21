d4085982b752f6873624e6997294b641c6de40c7
Khaled Waleed <khaledw62@gmail.com>
2026-06-21 21:50:07 +0300

    feat: privacy and policy

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -37,6 +37,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
   ];
 
   try {
